5e1ce1357129e1051807e27695119fa3063b33d7
AngelParra-png <angel6parra9fula7@gmail.com>
2026-09-23 22:04:41 -0500

    Create Usuario.java
    Creación de la clase usuario

---
+++ entities/Usuario.java
@@ -0,0 +1,34 @@
+public class Usuario {
+
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+
+    public Usuario(int idUsuario, String nombre, String apellido) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
