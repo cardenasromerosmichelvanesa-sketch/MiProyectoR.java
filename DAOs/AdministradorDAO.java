482307aa02a74f83081fadc812e78370649c2c8b
AngelParra-png <angel6parra9fula7@gmail.com>
2026-09-24 18:42:46 -0500

    Create AdministradorDAO.java
    Creación DAO administrador

---
+++ DAOs/AdministradorDAO.java
@@ -0,0 +1,21 @@
+import entities.Administrador;
+public class AdministradorDAO {
+    /*
+    * 
+    */
+
+    /* */
+    private void reporteVentas() {
+
+    }
+    private void gestionMesas() {
+        
+    }
+    private void gestionUsuarios() {
+
+    }
+    private void tiempoPreparacion() {
+
+    }
+
+}
