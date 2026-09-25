8adfa8d90f4ca8cd5fd5842d7a2189104bc9e67c
joaquinfelipe <joaquinfelipemunoz.15@gmail.com>
2026-09-25 00:56:26 -0500

    Create MeseroDAO.java

---
+++ DAOs/MeseroDAO.java
@@ -0,0 +1,33 @@
+import entities.Mesero;
+
+public class MeseroDAO {
+
+
+    private void get() {
+
+    }
+
+    private void set() {
+
+    }
+
+    private void crearPedido() {
+
+    }
+
+    private void agregarProductos() {
+
+    }
+
+    private void quitarProductos() {
+
+    }
+
+    private void asignarMesa() {
+
+    }
+
+    private void entregarPedido() {
+
+    }
+}
