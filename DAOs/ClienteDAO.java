9056db4f22d527609ec38504850348d437e514a6
joaquinfelipe <joaquinfelipemunoz.15@gmail.com>
2026-09-25 00:55:13 -0500

    Create ClienteDAO.java

---
+++ DAOs/ClienteDAO.java
@@ -0,0 +1,21 @@
+import entities.Cliente;
+
+public class ClienteDAO { 
+
+
+    private void get() {
+
+    }
+
+    private void set() {
+
+    }
+
+    private void formaPago() {
+
+    }
+
+    private void registra() {
+
+    }
+}
